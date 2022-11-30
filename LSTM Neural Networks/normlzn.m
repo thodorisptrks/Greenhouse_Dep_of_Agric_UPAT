0c2444205a571a9831247f39091fe6e71e95b887
thodoris777 <58725887+thodoris777@users.noreply.github.com>
2022-11-30 21:27:51 +0200

    Deleted

--- LSTM Neural Networks/normlzn.m
+++
@@ -1,8 +0,0 @@
-function [data_mins,data_maxes,data_norm] = normlzn(inputs,rmax,rmin)
-    
-    data_mins = min(inputs,[],1);
-    data_maxes = max(inputs,[],1);
-    
-    data_norm = ((rmax - rmin)*((inputs - data_mins)./(data_maxes - data_mins))) + rmin;
-
-end
